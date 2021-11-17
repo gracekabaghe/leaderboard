9853dec217ec888392235321eb9ae345d9993917
gracekabaghe <gracema2002@gmail.com>
2021-11-17 08:40:27 +0200

    edited a class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './style.css'
+import './style.css';
+import data from './scores.js';
+
+
+const allScores = document.querySelector('.score-details');
+console.log(allScores);
+for (let i = 0; i < data.length; i += 1) {
+    const oneScore = data[i];
+    allScores.innerHTML += `
+    <div class="score-card">
+    <p class="p-name score-elements">${oneScore.name}</p>
+    <p class="insert-score score-elements">${oneScore.score}</p>
+</div>
+      `;
+  }
